fb122a1f5a2694478aa05b6d771ccaee502231c7
Leslie Soto <leslie@leslies-macbook-pro.local>
2016-12-09 20:02:41 -0400

    Sign in validation complete. Missing alert messages and final sign in.

--- TapAndMunch/SignInViewController.swift
+++ TapAndMunch/SignInViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    // Dummy sign in parameters
+    let usernameActive = "user@tp.com"
+    let passwordActive = "12345"
+    
+    // Outlet variable initialization
     @IBOutlet weak var signInLabel: UILabel!
     @IBOutlet weak var signInImageView: UIImageView!
+    @IBOutlet weak var usernameInputTextField: UITextField!
+    @IBOutlet weak var passwordInputTextField: UITextField!
     
     // Initialization of UI
     override func viewDidLoad() {
@@ -24,6 +31,28 @@ class SignInViewController: UIViewController {
     // Unfinished
     @IBAction func signInPressed(_ sender: AnyObject) -> Void {
         print("Sign in tapped")
+
+        let usernameInput = usernameInputTextField.text
+        let passwordInput = passwordInputTextField.text
+        
+        // Both inputs exist
+        if usernameInput != nil && passwordInput != nil {
+            
+            print("Both inputs do exist")
+            
+            if (usernameInput == usernameActive) && (passwordInput == passwordActive){
+                // TO DO LOG IN PROCESS
+                print("Logging in")
+            }
+            else {
+                // TO DO Incorrect email or password
+                print("Incorrect email or password")
+            }
+        }
+        else {
+            // TO DO Display alert message
+            print("One or both inputs do not exist")
+        }
     }
 
     // Unfinished
