8459bc3a96bd1898e1261f887864da1452a8de74
Esther Rios <estherrios@esthers-macbook-pro.local>
2016-12-12 13:47:17 -0400

    AccountSettings and OrderHistory class added.

---
+++ TapAndMunch/SettingsCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsCell.swift
+//  TapAndMunch
+//
+//  Created by Esther Rios on 12/12/16.
+//  Copyright © 2016 HCI. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TapAndMunch/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  TapAndMunch
+//
+//  Created by Esther Rios on 12/12/16.
+//  Copyright © 2016 HCI. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
