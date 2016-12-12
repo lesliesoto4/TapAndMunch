0b707d285facb16125e6224b12371a5270507cba
Esther Rios <estherrios@esthers-macbook-pro.local>
2016-12-12 15:41:50 -0400

    PastOrder View Completed.

--- TapAndMunch/OrderHistoryViewController.swift
+++ TapAndMunch/OrderHistoryViewController.swift
@@ -8,17 +8,37 @@
 
 import UIKit
 
-class OrderHistoryViewController: UIViewController {
-
+class OrderHistoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let order_image = [UIImage(named: "pizzeria_donq"), UIImage(named: "off_the_wall")]
+    let order_rest = ["Pizzeria Don Quijote", "Off the Wall"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+        //return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "orderCell", for: indexPath) as! PastOrderCell
+        
+        cell.order_image.image = order_image[indexPath.row]
+        cell.order_rest_name.text = order_rest[indexPath.row]
+        
+        return (cell)
     }
 
 }
---
+++ TapAndMunch/PastOrderCell.swift
@@ -0,0 +1,27 @@
+//
+//  PastOrderCell.swift
+//  TapAndMunch
+//
+//  Created by Esther Rios on 12/12/16.
+//  Copyright © 2016 HCI. All rights reserved.
+//
+
+import UIKit
+
+class PastOrderCell: UITableViewCell {
+
+    @IBOutlet weak var order_image: UIImageView!
+    @IBOutlet weak var order_rest_name: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
