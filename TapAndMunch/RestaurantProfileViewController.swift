bbb21742b0f919bbf2072adee11ae0cffacb675a
Leslie Soto <leslie@leslies-macbook-pro.local>
2016-12-14 23:03:30 -0400

    Restaurant Profile dummy only missing Order Now button functionality.

--- TapAndMunch/RestaurantProfileViewController.swift
+++ TapAndMunch/RestaurantProfileViewController.swift
@@ -13,9 +13,6 @@ class RestaurantProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,5 +20,27 @@ class RestaurantProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func backToRestList(_ sender: AnyObject) {
+        // Get access to the storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // Get view controller from the storyboard
+        let restListView = storyboard.instantiateViewController(withIdentifier: "restList") as! RestaurantListViewController
+        
+        self.present(restListView, animated: true, completion: nil)
+    }
+    
+    @IBAction func orderRest(_ sender: AnyObject) {
+        // TO DO
+        
+    }
+    
+    @IBAction func accountEditButton(_ sender: AnyObject) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let accountView = storyboard.instantiateViewController(withIdentifier: "accountInfo") as! AccountViewController
+        
+        self.present(accountView, animated: true, completion: nil)
+    }
 }
 
